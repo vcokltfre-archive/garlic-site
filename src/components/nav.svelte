2e03eae84572d9b9a067881b6b28efcac1735237
vcokltfre <vcokltfre@gmail.com>
2022-03-19 21:55:40 +0000

    feat: initial commit

---
+++ src/components/nav.svelte
@@ -0,0 +1,57 @@
+<nav>
+  <img src="icon.svg" alt="Garlic Bot Icon" />
+  <span class="title">Garlic Bot</span>
+  <div class="grow" />
+  <a href="https://github.com/garlic-bot"><button>GitHub</button></a>
+  <a href="https://discord.gg/t5Bs4cXfv2"><button>Support</button></a>
+  <a
+    href="https://discord.com/api/oauth2/authorize?client_id=954718378755489852&permissions=314432&scope=bot%20applications.commands"
+    ><button>Invite</button></a
+  >
+</nav>
+
+<style lang="scss">
+  nav {
+    height: 2.5rem;
+    background-color: $col-bg-light;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    img {
+      height: 2rem;
+      border-radius: 25%;
+    }
+
+    span {
+      margin-left: 0.5rem;
+      font-size: 1.5rem;
+      font-weight: 500;
+      color: $col-fg-primary;
+    }
+
+    button {
+      height: 2.5rem;
+      background-color: $col-bg-light;
+      border: none;
+      color: $col-fg-primary;
+      padding: 0 0.5rem;
+      font-size: 1em;
+      cursor: pointer;
+    }
+
+    button:hover {
+      background-color: $col-bg-lighter;
+    }
+
+    button:active {
+      background-color: $col-bg-light;
+    }
+  }
+
+  @media (min-width: 768px) {
+    nav {
+      padding: 0 4rem;
+    }
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,66 @@
+<svelte:head>
+  <title>Home - Garlic Bot</title>
+</svelte:head>
+
+<div class="container">
+  <div class="garlic-bot">
+    <img src="icon.svg" alt="Garlic Bot Icon" />
+    <div class="garlic-text">
+      <h2>Garlic Bot</h2>
+      <a
+        href="https://discord.com/api/oauth2/authorize?client_id=954718378755489852&permissions=314432&scope=bot%20applications.commands"
+      >
+        <button>Add to Discord</button>
+      </a>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .container {
+    padding: 2rem 20%;
+    text-align: center;
+
+    .garlic-bot {
+      margin-top: 4rem;
+
+      img {
+        border-radius: 100%;
+        height: 10rem;
+      }
+
+      h2 {
+        font-size: 4rem;
+        font-weight: 500;
+        color: $col-fg-primary;
+        margin: 0;
+      }
+
+      button {
+        margin-top: 1rem;
+        padding: 0.5rem 1rem;
+        border: none;
+        border-radius: 0.5rem;
+        font-weight: 500;
+        font-size: 2rem;
+        cursor: pointer;
+        background-color: $col-bg-cta;
+        color: $col-fg-primary;
+      }
+
+      button:hover {
+        background-color: $col-bg-cta-hover;
+      }
+
+      button:active {
+        background-color: $col-bg-cta;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    .container {
+      padding: 1rem 2%;
+    }
+  }
+</style>
